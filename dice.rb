d435bf8c7285887c426f0755c211c28a1ad4994c
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-25 16:44:38 +0000

    formatting. completed pages

--- dice.rb
+++ dice.rb
@@ -1,9 +1,65 @@
 require "sinatra"
+require "sinatra/reloader"
 
 get("/") do
-  "Hello World"
+  "<h1>Dice Roll<h1>
+  <ul>
+    <li>Roll two 6-sided dice</li>
+    <li>Roll two 10-sided dice</li>
+    <li>Roll one 20-sided die</li>
+    <li>Roll five 4-sided dice</li>
+  </ul>"
 end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d6</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+
+  outcome = "You rolled a #{first_die}."
+
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+end
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  outcome = "You rolled a #{first_die},  #{second_die},  #{third_die},  #{fourth_die}, and #{fifth_die}, for a total of #{sum}."
+
+  "<h1>5d4</h1>
+  <p>#{outcome}</p>"
 end
