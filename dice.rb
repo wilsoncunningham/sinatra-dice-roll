96b08522c7170a5f1c91e0a61fceb3543932aa36
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-25 17:40:06 +0000

    adding wrapper to each

--- dice.rb
+++ dice.rb
@@ -28,7 +28,7 @@ get("/dice/2/6") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  erb(:two_six {:layout => :wrapper})
+  erb(:two_six, {:layout => :wrapper})
 end
 
 get("/dice/2/10") do
@@ -38,7 +38,7 @@ get("/dice/2/10") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  erb(:two_ten)
+  erb(:two_ten, {:layout => :wrapper})
 end
 
 get("/dice/1/20") do
