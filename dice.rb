1284eaf9d7646af1ad390647003f297d728e53ec
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-25 17:03:58 +0000

    refined. fixed quotations

--- dice.rb
+++ dice.rb
@@ -1,14 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 
 get("/") do
-  "<h1>Dice Roll<h1>
+  "
+  <h1>Dice Roll</h1>
   <ul>
-    <li>Roll two 6-sided dice</li>
-    <li>Roll two 10-sided dice</li>
-    <li>Roll one 20-sided die</li>
-    <li>Roll five 4-sided dice</li>
-  </ul>"
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
